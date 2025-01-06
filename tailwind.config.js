b61ff2b6aa66c0e72cb16e86642defe8f87540e0
Josef Válek <valek.josef@seznam.cz>
2025-01-06 19:13:22 +0100

    Small progress

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,10 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    fontFamily: {
+      serif: ["Cardo", "serif"],
+      display: ["Satisfy", "cursive"],
+    },
     extend: {},
   },
   plugins: [],
